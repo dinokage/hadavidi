7327aa18e42703e30ff1a6f30d349073bde95c85
dinokage <yerradinesh01@gmail.com>
2021-10-29 12:33:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cons');
+const PORT = process.env.PORT || 8000;
+console.log("boom boom");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cors());
+app.listen(PORT, ()=> {console.log("hadavidi is running");});
+
+
