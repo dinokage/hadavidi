b252f7fbe14327e96c7bff9ff3a1679b73611fad
dinokage <yerradinesh01@gmail.com>
2021-10-29 15:02:35 +0530

    .

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(cors());
-app.listen(PORT, ()=> {console.log("hadavidi is running");});
+app.listen(PORT, ()=> {
+    console.log("server started");
+    }
+);
+
 
 
